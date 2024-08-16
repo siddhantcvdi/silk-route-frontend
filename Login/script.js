a92163f3af0b0f371ac50ebb1be582875c68f38c
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-08-16 19:36:32 +0530

    Changed logic of login page

--- Login/script.js
+++ Login/script.js
@@ -1,24 +1,22 @@
 "use strict"
 const inputHTML= '<input type="text" class="input-field name-field" placeholder="Name">'
-const newAcc = document.querySelector('.new-acc')
-const backToLogin = document.querySelector('.back-login')
+const btn = document.querySelector('#btnLoginReg')
+const changeMode = document.querySelector('.change-mode');
+let currentMode = 'login';
 
-document.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('new-acc')){
-        document.querySelector('.input-container').insertAdjacentHTML('afterbegin',inputHTML)
-        document.querySelector('#btnLogin')?.remove();
-        document.querySelector('.input-container').insertAdjacentHTML('afterend','<button id="btnRegister">Register</button>')
-        newAcc.textContent = "Back to Login"
-        newAcc.classList.remove('new-acc')
-        newAcc.classList.add('back-login')
+changeMode.addEventListener('click', () => {
+    if(currentMode === 'login'){
+        currentMode = 'register';
+        changeMode.textContent = 'Already have an account?';
+        btn.textContent = 'Login';
+        document.querySelector('.input-container').insertAdjacentHTML('afterbegin', inputHTML);
     }
-    else if(e.target.classList.contains('back-login')){
+    else{
+        currentMode = 'login';
+        changeMode.textContent = 'Create a new account?';
+        btn.textContent = 'Register';
         document.querySelector('.name-field').remove();
-        document.querySelector('#btnRegister')?.remove();
-        document.querySelector('.input-container').insertAdjacentHTML('afterend','<button id="btnLogin">Login</button>')
-        newAcc.textContent = "Create a new Account"
-        newAcc.classList.remove('back-login')
-        newAcc.classList.add('new-acc')
     }
+    console.log(currentMode);
+    
 })
-console.log('Hi');
