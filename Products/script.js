a9c728f5208dee9aef613d1705df37170be067f2
Siddhant <siddhantchaturvedi2004@gmail.com>
2024-08-16 19:20:38 +0530

    Added More details button

--- Products/script.js
+++ Products/script.js
@@ -2,22 +2,16 @@
 
 const modal_blur = document.querySelector('.modal-blur');
 const modal = document.querySelector('.modal')
-const product_cards = document.querySelectorAll('.product-card');
-
-product_cards.forEach((product_card)=>{
-    product_card.addEventListener('click',()=>{
-        modal_blur.classList.remove('hide');
-    })
-})
-
-document.querySelector('.product-button').addEventListener('click',(e)=>{
-    e.stopPropagation();
-    console.log('Button');
-    
-})
-
+const product_button = document.querySelectorAll('.product-button');
 
 document.addEventListener('click',(e)=>{
     if(e.target.classList.contains('modal-blur'))
         modal_blur.classList.add('hide')
+})
+
+product_button.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.stopPropagation();
+        modal_blur.classList.remove('hide');
+    })
 })
